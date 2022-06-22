981ae40d8e7c5f6db4a2c767c107edebfeb67c4a
Ayoub <unjde@student.kit.edu>
2022-06-22 19:27:44 +0200

    Fix models.profile directory structure

--- core/models/profile/Profile.ts
+++ core/models/profile/Profile.ts
@@ -1,8 +1,35 @@
 import { HIndex } from '../profile/HIndex'
 import { I10Index } from '../profile/I10Index'
 
-export class Profile{}
-export class BasicProfile{}
+export interface Profile {}
+export class BasicProfile implements Profile {
+
+  private _id: number;
+  private _name: string;
+  private _affiliation: string;
+  private _totalCitations: number;
+
+  constructor(id: number) {
+    this._id = id;
+  }
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get affiliation(): string {
+    return this._affiliation;
+  }
+
+  public get totalCitations(): number {
+    return this._totalCitations;
+  }
+
+}
 export class FullProfile{
     private _basicProfile: BasicProfile;
     private _hIndex: HIndex;
