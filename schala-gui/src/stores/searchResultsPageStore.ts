73ef3e932ed94dc4343ad47ce7ca761f7e7a3374
Yigit <yigit.oguz@student.kit.edu>
2022-06-23 19:03:35 +0300

    Fix

---
+++ schala-gui/src/stores/searchResultsPageStore.ts
@@ -0,0 +1,68 @@
+import { BasicProfile } from 'app/../core/models/profile/Profile';
+import { SearchResultsFactory } from '../../../core/factories/SearchResultsFactory'
+import { SearchResultsModel } from '../../../core/models/searchresultsmodel/SearchResultsModel'
+import { defineStore } from 'pinia';
+
+interface ISearchResultsPageStore {
+    setSearchString: (passedSearchString: string) => void;
+    getSearchString: () => string;
+    getSearchResultsFactory: () => SearchResultsFactory;
+    getSearchResultsShowingModel: () => SearchResultsModel;
+    setSearchResultsShowingModel: (model: SearchResultsModel) => void;
+    getSearchResultsCachedModel: () => SearchResultsModel;
+    setSearchResultsCachedModel: (model: SearchResultsModel) => void;
+    resetFromCache: () => void;
+    applyAllFilters: () => void;
+}
+export const searchResultsStore = defineStore<string, ISearchResultsPageStore>('searchResultsPage', () => {
+  let searchString = '';
+  const searchResultsFactory:SearchResultsFactory = {} as SearchResultsFactory;
+  let searchResultsShowingModel: SearchResultsModel = {} as SearchResultsModel;
+  let searchResultsCachedModel: SearchResultsModel = {} as SearchResultsModel;
+
+  function getSearchString(): string {
+    return searchString;
+  }
+  // TODO: Fix setSearchString after SearchResultsModel and deepCopy are implemented
+  function setSearchString(passedSearchString: string):void {
+    searchString = passedSearchString;
+    console.log(searchString)
+    const profile: Array<BasicProfile> = getSearchResultsFactory().build(passedSearchString);
+    searchResultsCachedModel = new SearchResultsModel(profile);
+    searchResultsShowingModel = searchResultsShowingModel.deepCopy();
+  }
+  function getSearchResultsFactory():SearchResultsFactory {
+    return searchResultsFactory;
+  }
+  function getSearchResultsShowingModel():SearchResultsModel {
+    return searchResultsShowingModel;
+  }
+  function setSearchResultsShowingModel(model: SearchResultsModel) {
+    searchResultsShowingModel = model;
+  }
+  function getSearchResultsCachedModel():SearchResultsModel {
+    return searchResultsCachedModel;
+  }
+  function setSearchResultsCachedModel(model: SearchResultsModel) {
+    searchResultsCachedModel = model;
+  }
+  // TODO: Implement resetFromCache
+  function resetFromCache(): void {
+    return ;
+  }
+  // TODO: Implement applyAllFilters
+  function applyAllFilters(): void {
+    return ;
+  }
+  return {
+    setSearchString,
+    getSearchString,
+    getSearchResultsFactory,
+    getSearchResultsShowingModel,
+    setSearchResultsShowingModel,
+    getSearchResultsCachedModel,
+    setSearchResultsCachedModel,
+    resetFromCache,
+    applyAllFilters
+  }
+});
