575c1aa45fc41ee9f353ad5cb2820347110b99f2
Yigit <yigit.oguz@student.kit.edu>
2022-06-24 01:45:59 +0300

    Add Input interface

--- core/src/factories/Factory.ts
+++ core/src/factories/Factory.ts
@@ -1,3 +1,3 @@
-export interface Factory {
+export abstract class Factory {
     
 }
--- core/src/factories/ProfileFactory.ts
+++ core/src/factories/ProfileFactory.ts
@@ -3,6 +3,7 @@ import { FullProfile } from '../models/profile/Profile'
 export class ProfileFactory {
 
     build(authorId: string):  FullProfile[] {
+        authorId;
         return {} as FullProfile[]
     }
 }
