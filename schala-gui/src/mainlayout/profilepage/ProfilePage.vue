75bbb559ea3a8e3e3022e9d1255703d9acb4d8bb
BarisUmutKizilcelik <bukizilcelik@gmail.com>
2022-06-23 21:14:36 +0200

    Create ProfilePage

---
+++ schala-gui/src/mainlayout/profilepage/ProfilePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row justify-center bg-transparent">
+    <div class="col-md-12 col-xs-12 self-center">
+      <ProfileSummary />
+    </div>
+  </div>
+  <ProfileContent />
+</template>
+<script setup charset="utf-8">
+import { profilePageStore } from 'src/stores/profilePageStore';
+import ProfileSummary from 'src/sharedcomponents/ProfileSummary.vue';
+import ProfileContent from './ProfileContent.vue';
+
+const getProfilePageStore = () => {
+  return profilePageStore();
+}
+
+const routerView = () => {
+      //TODO: Implement routerView
+
+}
+
+</script>
