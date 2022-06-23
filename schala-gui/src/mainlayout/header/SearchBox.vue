a4c4a71a99b8048f1b5da4632e50f52c8fd82a63
Ulas Uyanik <ulasuyanik.35@gmail.com>
2022-06-23 20:39:07 +0200

    Implement SearchBox, StickyHeader, ComparePage

---
+++ schala-gui/src/mainlayout/header/SearchBox.vue
@@ -0,0 +1,14 @@
+<template>
+ <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+  import {ref} from 'vue'
+  import SearchResultsPage from '../searchresultspage/SearchResultsPage.vue';
+  import { useRouter } from 'vue-router'
+  let searchString : string
+  const router = useRouter();
+  let handleSearch = async (_) => {
+    router.push({ path: '/profile/search' });
+  };
+</script>
---
+++ schala-gui/src/mainlayout/header/StickyHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="../assets/wisteria_Background_only_plant.png">
+          </q-avatar>
+          schala
+        </q-toolbar-title>
+        <q-input dark dense standout v-model="text" input-class="text-left" class="float-right q-mb-sm q-mt-sm">
+          <template v-slot:append>
+            <q-icon v-if="text === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
+          </template>
+        </q-input>
+      </q-toolbar>
+
+      <q-tabs align="center" animated swipeable class="desktop-hide">
+        <q-route-tab to="/profile/search" label="Search Results" icon="search" />
+        <q-route-tab to="/profile/show" label="Profile" icon="person" />
+        <q-route-tab to="/profile/compare" label="Compare" icon="people" />
+      </q-tabs>
+      <q-tabs align="center" animated swipeable class="mobile-hide" inline-label>
+        <q-route-tab to="/profile/search" label="Search Results" icon="search" />
+        <q-route-tab to="/profile/show" label="Profile" icon="person" />
+        <q-route-tab to="/profile/compare" label="Compare" icon="people" />
+      </q-tabs>
+    </q-header>
+</template>
+
+<script setup lang="ts">
+  import {ref} from 'vue'
+  let text = ref('')
+</script>
