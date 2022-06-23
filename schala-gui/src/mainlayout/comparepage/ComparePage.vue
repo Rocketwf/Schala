4473983b528a1cb11676f34dc71294d9c8092888
Ulas Uyanik <ulasuyanik.35@gmail.com>
2022-06-23 13:19:11 +0200

    copy the template from the mock-up

---
+++ schala-gui/src/mainlayout/comparepage/ComparePage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row justify-center" style="width: 100%">
+    <div class="col-md-3 col-sm-12 self-center">
+      <profile-basic
+        name="Walter F. Tichy"
+        size="3"
+        scholar_id="23RPQBQAAAAJ"
+        affiliation="KIT"
+        button_class="negative"
+        profile_pic="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=23RPQBQAAAAJ&citpid=2"
+        button_type="Delete"
+      />
+    </div>
+    <div class="col-md-3 col-sm-12 self-center">
+      <profile-basic
+        name="Gregor Snelting"
+        size="3"
+        scholar_id="sprhcYcAAAAJ"
+        affiliation="KIT"
+        button_class="negative"
+        profile_pic="https://scholar.google.com/citations/images/avatar_scholar_128.png"
+        button_type="Delete"
+      />
+    </div>
+    <div class="col-md-3 col-sm-12 self-center">
+      <profile-basic
+        name="Klemens Böhm"
+        size="3"
+        scholar_id="RzCtTjYAAAAJ"
+        affiliation="KIT"
+        button_class="negative"
+        profile_pic="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=RzCtTjYAAAAJ&citpid=2"
+        button_type="Delete"
+      />
+    </div>
+    <div class="col-md-3 col-sm-12 self-center">
+      <profile-basic
+        name="Mehdi B. Tahoori"
+        size="3"
+        scholar_id="NBQ_casAAAAJ"
+        affiliation="KIT"
+        button_class="negative"
+        profile_pic="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=NBQ_casAAAAJ&citpid=2"
+        button_type="Delete"
+      />
+    </div>
+  </div>
+</template>
+<script lang = "ts">
+
+import { ref, defineComponent } from 'vue'
+import {} from '../../sharedcomponents/ProfileSummary.vue'
+import { profilePageStore } from '../../stores/profilePageStore'
+
+</script>
