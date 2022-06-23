8144b8080797f0add639e6978c519de6869f3659
Magikarpm <58331809+Magikarpm@users.noreply.github.com>
2022-06-23 23:53:41 +0200

    Create method signatures for profile factory

--- core/src/factories/ProfileFactory.ts
+++ core/src/factories/ProfileFactory.ts
@@ -1,8 +1,25 @@
 import { FullProfile } from '../models/profile/Profile'
-
+import { HIndex } from '../models/profile/Profile'
+import { I10Index } from '../models/profile/Profile'
 export class ProfileFactory {
 
     build(authorId: string):  FullProfile[] {
         return {} as FullProfile[]
+    }
+
+    calculateHIndex(): HIndex {
+        return null
+    }
+
+    calculateI10Index(): I10Index {
+        return null
+    }
+
+    calculateSelfCitations(): number {
+        return 0
+    }
+
+    calculateIndirectSelfCitations(): number {
+        return 0
     }
 }
