9b87017213cc0b1cd346dca976b9ae1b993254bd
Yigit <yigit.oguz@student.kit.edu>
2022-06-24 18:22:18 +0300

    P50 Add basic test for ProfileFactory

--- core/src/__tests__/factories/ProfileFactoryTest.ts
+++ core/src/__tests__/factories/ProfileFactoryTest.ts
@@ -1,5 +1,12 @@
-describe('profile factory test', () => {
-    it('passes', () => {
-        expect(true).toBe(true);
+import { ProfileFactory } from '../../index';
+
+describe('profile factory build test with tichy', () => {
+    it('Tichy`s profile must be built correctly', async () => {
+        const testFactory = new ProfileFactory();
+        const tichyProfile = testFactory.build('1679754')[0];
+        expect(tichyProfile.hIndex.hIndex).toBe(39);
+        expect(tichyProfile.basicProfile.name).toBe('W. Tichy');
+        expect(tichyProfile.basicProfile.id).toBe('1679754');
+        expect(tichyProfile.basicProfile.totalCitations).toBe(7903);
     });
 });
