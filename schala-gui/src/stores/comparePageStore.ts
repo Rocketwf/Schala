c069edaa2830d8269ec0b3a2bb959632639b2adc
yagmur <yagmurdilaraakalin@gmail.com>
2022-06-24 15:07:31 +0200

    Implement comparePageStore

--- schala-gui/src/stores/comparePageStore.ts
+++ schala-gui/src/stores/comparePageStore.ts
@@ -1,22 +1,58 @@
 import { defineStore } from 'pinia';
+import { FullProfile } from 'schala-core';
+// import { profilePageStore } from './profilePageStore';
+
 
 export const comparePageStore = defineStore('comparePage', () => {
 
-    function addProfile(profileId: string) {
-        return //TODO: Implement this
-    }
+  let fullProfiles: FullProfile[] = [];
+  // const comparisonRepresentation: ComparisonRepresentation = new ComparisonRepresentation({} as FullProfile);
+  // const profilePageStore: ProfilePageStore = new ProfilePageStore();
 
-    function removeProfile(profileId: string) {
-        return //TODO: Implement this
-    }
-    
-    function isBeingCompared(profileId: string) {
-        return false //TODO: Implement this
-    }
 
-    return {
-        isBeingCompared,
-        addProfile,
-        removeProfile
+  function addProfile(profileId: string) {
+    if (fullProfiles.length > 4) {
+      return;
     }
+    const fullProfile = {} as FullProfile;
+    fullProfiles.push(fullProfile);
+    return
+  }
+
+  function removeProfile(profileId: string) {
+    if (fullProfiles.length == 0) {
+      return;
+    }
+    fullProfiles = fullProfiles.filter(p => p.basicProfile.id !== profileId);
+  }
+
+  function isBeingCompared(profileId: string) {
+    for (const profile of fullProfiles) {
+      if (profile.basicProfile.id === profileId) {
+        return true;
+      }
+    }
+    return false
+  }
+
+  function getFullProfiles(): FullProfile[] {
+    return fullProfiles;
+  }
+
+  // function getComparisonRepresentation() : ComparisonRepresentation {
+  //   return comparisonRepresentation;
+  // }
+
+  // function getProfilePageStore() : ProfilePageStore {
+  //   return profilePageStore;
+  // }
+
+  return {
+    isBeingCompared,
+    addProfile,
+    removeProfile,
+    getFullProfiles,
+    // getComparisonRepresentation,
+    // getProfilePageStore
+  }
 });
