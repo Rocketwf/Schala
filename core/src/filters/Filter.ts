e3c2babb9d04b09b3c064c7ce92c2d7dd55f0027
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-06-25 23:42:47 +0200

    Implement TextField and Filter

--- core/src/filters/Filter.ts
+++ core/src/filters/Filter.ts
@@ -1,5 +1,9 @@
 import { Filterable } from './Filterable';
 
 export abstract class Filter<S, T extends Filterable<T>> {
-    value: S;
+    private _value: S;
+
+    public set value(newValue: S) {
+        this._value = newValue;
+    }
 }
