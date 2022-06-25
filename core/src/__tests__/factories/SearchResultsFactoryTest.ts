fdde4b33f7d8eb4968fd643c1a102428c689ded0
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-06-25 19:52:00 +0200

    Fix SearchResultsFactory test

--- core/src/__tests__/factories/SearchResultsFactoryTest.ts
+++ core/src/__tests__/factories/SearchResultsFactoryTest.ts
@@ -1,19 +1,10 @@
 import { SearchResultsFactory, BasicProfile } from '../../index';
 
 describe('builds Tichy`s profile correctly', () => {
-    it('must build Tichy`s profile correctly', async () => {
+    it('must find profiles for query walter tichy correctly', async () => {
+        const walterTichy: BasicProfile = new BasicProfile('1679754', 'W. Tichy', [], 7903);
         const testFactory: SearchResultsFactory = new SearchResultsFactory();
         const basicProfiles: BasicProfile[] = await testFactory.build('walter tichy');
-        let basicProfile: BasicProfile;
-        for (basicProfile of basicProfiles) {
-            if (
-                basicProfile.name == 'W. Tichy' &&
-                basicProfile.affiliation == [] &&
-                basicProfile.totalCitations == 7903
-            ) {
-                return true;
-            }
-        }
-        return false;
+        expect(basicProfiles).toContainEqual(walterTichy);
     });
 });
