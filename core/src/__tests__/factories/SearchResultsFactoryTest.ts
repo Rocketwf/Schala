8820686d0202c9556061b39c0724e092f9e39b6d
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-06-25 18:44:14 +0200

    Implement SearchResultsFactoryTest

--- core/src/__tests__/factories/SearchResultsFactoryTest.ts
+++ core/src/__tests__/factories/SearchResultsFactoryTest.ts
@@ -1,13 +1,19 @@
 import { SearchResultsFactory, BasicProfile } from '../../index';
 
 describe('builds Tichy`s profile correctly', () => {
-    it('Tichy`s profile must be built correctly', async () => {
+    it('must build Tichy`s profile correctly', async () => {
         const testFactory: SearchResultsFactory = new SearchResultsFactory();
-        testFactory.build('walter tichy').then((fullProfiles: BasicProfile[]) => {
-            const tichyProfile: BasicProfile = fullProfiles[1];
-            expect(tichyProfile.name).toBe('W. Tichy');
-            expect(tichyProfile.affiliation).toStrictEqual([]);
-            expect(tichyProfile.totalCitations).toBe(7903);
-        });
+        const basicProfiles: BasicProfile[] = await testFactory.build('walter tichy');
+        let basicProfile: BasicProfile;
+        for (basicProfile of basicProfiles) {
+            if (
+                basicProfile.name == 'W. Tichy' &&
+                basicProfile.affiliation == [] &&
+                basicProfile.totalCitations == 7903
+            ) {
+                return true;
+            }
+        }
+        return false;
     });
 });
