4aa5daf7019e259b64f996879232a4df85039896
ulas uyanik <ulasuyanik.35@gmail.com>
2022-06-26 11:04:01 +0200

    Implement HeaderLogo

---
+++ schala-gui/src/mainlayout/header/HeaderLogo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="headerLogo">
+        <q-form @submit="redirectHome"></q-form>
+        <img src="../../assets/wisteria_Background_only_plant.png" />
+    </div> 
+</template>
+<script setup lang="ts">
+    import { useRouter, Router } from 'vue-router';
+     const router: Router = useRouter();
+     const redirectHome = () => {
+       router.replace("#")
+};
+</script>
