af25dae1636a9e9525ab988b05ec73c90c83f7e6
Yigit <yigit.oguz@student.kit.edu>
2022-06-26 22:19:48 +0200

    Fix error in SearchResultsPage

--- schala-gui/src/mainlayout/searchresultspage/SearchResultsPage.vue
+++ schala-gui/src/mainlayout/searchresultspage/SearchResultsPage.vue
@@ -2,7 +2,7 @@
     <div class="row justify-center">
         <div class="col-xs-12 col-md-8">
             <!-- <q-page padding>          Add this after implementing main layout -->
-            <q-input rounded standout bottom-slots v-model="affiliationFilter" label="Filter by Affiliation" @update:model-value="handleAffiliationFilter">
+            <q-input rounded standout bottom-slots v-model="affiliationFilter" label="Filter by Affiliation" :rules="[data => typeof data === 'string']" @update:model-value="handleAffiliationFilter">
                 <template v-slot:prepend>
                     <q-icon name="school" />
                 </template>
@@ -39,8 +39,8 @@ const getSearchResultsPageStore = () => {
     return searchStore;
 }
 
-const handleAffiliationFilter = (data: string): void =>  {
-    affiliationFilter = data;
+const handleAffiliationFilter = (data: string | number | null): void =>  {
+    affiliationFilter = data as string;
     getSearchResultsPageStore().setAffiliationFilter(getAffiliationFilter());
 }
 
