af6db22be05062923da9baa4258a0e1730cd99f8
Yigit <yigit.oguz@student.kit.edu>
2022-06-26 13:32:56 +0200

    Fix AffiliationFilter

--- core/src/filters/searchresultsfilters/SearchResultsFilter.ts
+++ core/src/filters/searchresultsfilters/SearchResultsFilter.ts
@@ -9,10 +9,18 @@ export class AffiliationFilter extends SearchResultsFilter<string> {
     apply(model: SearchResultsModel): void {
         const toFilter: Array<BasicProfile> = model.profiles;
         const filtered: Array<BasicProfile> = toFilter.filter((profile: BasicProfile) => {
-            if (profile.affiliation.indexOf(this.value) !== -1) {
+            if (this.contains(profile.affiliation, this.value).length !== 0) {
                 return true;
             }
         });
         model.profiles = filtered;
+    }
+
+    private contains(array: Array<string>, value: string): Array<string> {
+        return array.filter((element: string) => {
+            if (element.indexOf(value) !== -1) {
+                return true;
+            }
+        });
     }
 }
