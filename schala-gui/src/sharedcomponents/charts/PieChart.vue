0fbfdcedf8f20c088f8a6de96d17ef689944ccda
Ayoub <unjde@student.kit.edu>
2022-06-28 07:17:22 +0200

    Clean up and add a type declaration to opt
    follow down the type of the opt in the PieChart chartOptions formatter

--- schala-gui/src/sharedcomponents/charts/PieChart.vue
+++ schala-gui/src/sharedcomponents/charts/PieChart.vue
@@ -1,66 +1,65 @@
 <template>
-  <div id="chart">
-    <apexchart type="pie" width="370" :options="chartOptions" :series="getSeries()"></apexchart>
-  </div>
+    <div id="chart">
+        <apexchart type="pie" width="370" :options="chartOptions" :series="getSeries()"></apexchart>
+    </div>
 </template>
 <script setup charset="utf-8" lang="ts">
-import { PieChartModel, Series } from 'schala-core';
+import { PieChartModel } from 'schala-core';
 const props = defineProps<{
-  pieChartModel: PieChartModel
+    pieChartModel: PieChartModel;
 }>();
 
 const getSeries = () => {
-  const apexSeries: Array<number> = new Array<number>();
-  for(const serie of props.pieChartModel.series) {
-    apexSeries.push(...serie.data);
-
-  }
-  return apexSeries;
-}
+    const apexSeries: Array<number> = new Array<number>();
+    for (const serie of props.pieChartModel.series) {
+        apexSeries.push(...serie.data);
+    }
+    return apexSeries;
+};
 
 const getLabels = () => {
-  const apexLabels: Array<string> = new Array<string>();
-  for(const serie of props.pieChartModel.series) {
-    apexLabels.push(serie.name);
+    const apexLabels: Array<string> = new Array<string>();
+    for (const serie of props.pieChartModel.series) {
+        apexLabels.push(serie.name);
+    }
+    return apexLabels;
+};
 
-  }
-  return apexLabels;
-}
-
-        const chartOptions = {
-          dataLabels: {
-            enabled: true,
-            enabledOnSeries: undefined,
-            formatter: function(value:number, { seriesIndex, _, w }:any) {
-              return w.config.series[seriesIndex] + ' (' + Number(value).toFixed(2) + '%)'
-            }
-          },
-          chart: {
-            width: 380,
-            type: 'pie',
-            toolbar: {
-              offsetX: -52,
-              offsetY: -52,
-              show: true,
-              tools: {
-                download: '<i class="q-icon notranslate material-icons" aria-hidden="true" role="presentation" style="font-size: 24px;">download</i>',
-              },
+type ApexOptionsType = { seriesIndex: number; w: { config: { series: Array<number> } } };
+const chartOptions = {
+    dataLabels: {
+        enabled: true,
+        enabledOnSeries: undefined,
+        formatter: function (value: number, opts: ApexOptionsType) {
+            return opts.w.config.series[opts.seriesIndex] + ' (' + Number(value).toFixed(2) + '%)';
+        },
+    },
+    chart: {
+        width: 380,
+        type: 'pie',
+        toolbar: {
+            offsetX: -52,
+            offsetY: -52,
+            show: true,
+            tools: {
+                download:
+                    '<i class="q-icon notranslate material-icons" aria-hidden="true" role="presentation" style="font-size: 24px;">download</i>',
             },
-          },
-          labels: getLabels(),
-                  
-          legend: {
-              show: true,
-              position: 'bottom',
-              fontSize: '13px',
-          },
-          plotOptions: {
-            pie: {
-              dataLabels: {
-                offset: -20,
-              },
-           }
-         }
-        }
+        },
+    },
+    labels: getLabels(),
 
+    legend: {
+        show: true,
+        position: 'bottom',
+        fontSize: '13px',
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                offset: -20,
+            },
+        },
+    },
+};
 </script>
