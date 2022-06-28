c92d0b483018813fb7be982793e5fd91d6b39708
Emirhan Yilmaz <uluqq@student.kit.edu>
2022-06-28 11:56:37 +0200

    Fix ComparePage

--- schala-gui/src/mainlayout/comparepage/ComparePage.vue
+++ schala-gui/src/mainlayout/comparepage/ComparePage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="row justify-center bg-transparent">
-      <div v-for="profile of profileArray"
+      <div
+v-for="profile of (store.fullProfiles as FullProfile[])"
       :key="profile.basicProfile.id">
         <div class="col-md-4 col-xs-12 self-center">
           <ProfileSummary :profile="profile"/>
@@ -14,8 +15,7 @@
 import ProfileSummary from '../../sharedcomponents/ProfileSummary.vue';
 import CompareContent from './CompareContent.vue';
 import { comparePageStore } from '../../stores/comparePageStore'
-import { BasicProfile, FullProfile } from 'schala-core';
-import { HIndex, I10Index } from 'schala-core/dist/models/profile';
+import { FullProfile } from 'schala-core';
 
 const getComparePageStore = () => {
     return comparePageStore;
@@ -29,18 +29,4 @@ getProfileSummary;
 
 const store = comparePageStore();
 
-//Cloning of the full profile array in the compare page store because the objects delivered by the store are not really full profiles
-//but weird readonly objects. Extensive googling yielded no results :(
-let profileArray: FullProfile[] = new Array<FullProfile>();
-for(const profile of store.fullProfiles) {
-  profileArray.push( new FullProfile (
-    new BasicProfile(profile.basicProfile.id, profile.basicProfile.name, profile.basicProfile.affiliation, profile.basicProfile.totalCitations),
-    new HIndex(profile.hIndex.hIndex, profile.hIndex.hIndexWithoutSelfCitations),
-    new I10Index(profile.i10Index.i10Index, profile.i10Index.i10IndexWithoutSelfCitations)
-    )
-  )
-}
-//For testing purposes, delete these 2 lines
-profileArray = new Array<FullProfile>();
-profileArray.push(new FullProfile(new BasicProfile('a', 'b', [], 10), new HIndex(5, 6), new I10Index(7, 8)))
 </script>
