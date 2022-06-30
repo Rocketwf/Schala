713ba576457885116a5f9235d055c3a248de52ef
Ayoub <unjde@student.kit.edu>
2022-06-30 03:13:46 +0200

    Slice Articles for development

--- schala-gui/src/sharedcomponents/cards/ArticlesCard.vue
+++ schala-gui/src/sharedcomponents/cards/ArticlesCard.vue
@@ -1,7 +1,7 @@
 <template>
     <simple-card :simple-card-model="props.cardModel">
             <article-item
-                v-for="art in props.cardModel.articles" 
+                v-for="art in getArticles()"
                 :key="art.id"
                 :article="art"
             />  
@@ -9,12 +9,16 @@
 </template>
 
 <script setup charset="utf-8" lang="ts">
-import { ArticlesModel } from 'schala-core'
+import { ArticlesModel, Article } from 'schala-core'
 import ArticleItem from '../../mainlayout/profilepage/ArticleItem.vue'
 import SimpleCard from './SimpleCard.vue'
 
 const props = defineProps<{
     cardModel: ArticlesModel,
 }>()
+
+const getArticles = (): Article[] => {
+  return props.cardModel.articles.slice(0, 10);
+}
 
 </script>
