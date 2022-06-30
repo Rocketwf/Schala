0051612e1004c8284a14038cb0bf103b0d79acd6
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-06-30 15:16:02 +0200

    Implement ChartOptionModel and ChartOptionsFIlter

--- core/src/filters/chartoptionsfilters/ChartOptionFilter.ts
+++ core/src/filters/chartoptionsfilters/ChartOptionFilter.ts
@@ -1,3 +1,21 @@
-export abstract class ChartOptionFilter<S> {
-    abstract apply(model: ChartOptionFilter<S>): void;
+import { ChartOptionsModel } from '../../models';
+import { Filter } from '../Filter';
+
+export abstract class ChartOptionFilter<S> extends Filter<S, ChartOptionsModel> {
+    abstract apply(model: ChartOptionsModel): void;
+}
+
+export class ScaleUpFilter extends ChartOptionFilter<boolean> {
+    apply(model: ChartOptionsModel): void {
+        if (this.value) {
+            let limits: number[];
+            for (const serie of model.series) {
+                const sum: number = serie.data.reduce((accumulator: number, current: number) => {
+                    return accumulator + current;
+                }, 0);
+                limits.push(sum);
+            }
+            model.maxLimit = Math.max(...limits);
+        }
+    }
 }
