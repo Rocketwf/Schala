ab95e1918b79436df5a8411744c81bc369e4b27f
yagmur <yagmurdilaraakalin@gmail.com>
2022-07-01 16:51:58 +0200

    Fix ObejctSeriesChartModel

--- core/src/models/objectserieschartmodel/ObjectSeriesChartModel.ts
+++ core/src/models/objectserieschartmodel/ObjectSeriesChartModel.ts
@@ -1,8 +1,41 @@
 import { Filter, Filterable } from '../../filters';
+import { SimpleCardModel, ViewName } from '../simplecardmodel';
 
-export class ObjectSeriesChartModel implements Filterable<ObjectSeriesChartModel> {
-    filters: Filter<object, ObjectSeriesChartModel>[];
+export abstract class ObjectSeriesChartModel implements Filterable<ObjectSeriesChartModel>, SimpleCardModel {
+    private _id: string;
+    private _colWidth: number;
+    private _title: string;
+    private _sub: string;
+    private _viewName: ViewName;
+    private _filters: Filter<number, ObjectSeriesChartModel>[];
     private _series: Series[];
+    private _xTitle: string;
+    private _yTitle: string;
+    private _labels: string[];
+
+    constructor(
+        _title: string,
+        _sub: string,
+        _viewName: ViewName,
+        _colWidth: number,
+        _series: Array<Series>,
+        _xTitle: string,
+        _yTitle: string,
+        _labels: string[],
+    ) {
+        this._title = _title;
+        this._sub = _sub;
+        this._viewName = _viewName;
+        this._colWidth = _colWidth;
+        this._series = _series;
+        this._xTitle = _xTitle;
+        this._yTitle = _yTitle;
+        this._labels = _labels;
+    }
+
+    abstract deepCopy(): ObjectSeriesChartModel;
+
+    abstract applyAllFilters(): void;
 
     public get series(): Series[] {
         return this._series;
@@ -12,11 +45,44 @@ export class ObjectSeriesChartModel implements Filterable<ObjectSeriesChartModel
         this._series = newSeries;
     }
 
-    deepCopy(): ObjectSeriesChartModel {
-        throw new Error('Method not implemented.');
+    public get filters(): Filter<number, ObjectSeriesChartModel>[] {
+        return this._filters;
     }
-    applyAllFilters(): void {
-        throw new Error('Method not implemented.');
+
+    public set filters(newFilters: Filter<number, ObjectSeriesChartModel>[]) {
+        this._filters = newFilters;
+    }
+
+    public get colWidth(): number {
+        return this._colWidth;
+    }
+
+    public get title(): string {
+        return this._title;
+    }
+
+    public get id(): string {
+        return this._id;
+    }
+
+    public get sub(): string {
+        return this._sub;
+    }
+
+    public get viewName(): ViewName {
+        return this._viewName;
+    }
+
+    public get xTitle(): string {
+        return this._xTitle;
+    }
+
+    public get yTitle(): string {
+        return this._yTitle;
+    }
+
+    public get labels(): string[] {
+        return this._labels;
     }
 }
 
@@ -36,5 +102,8 @@ export class Series {
     }
     public get data(): Array<number> {
         return this._data;
+    }
+    public get type(): string {
+        return this._type;
     }
 }
