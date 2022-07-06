5badcbfde0c25a18e101c5ed92bf0f02983ac047
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-07-07 00:41:44 +0200

    Implement SearchResultsService and BasicProfile

--- server/src/services/SearchResultsService.ts
+++ server/src/services/SearchResultsService.ts
@@ -1,9 +1,23 @@
+import { DataSource } from '../datasources';
+import { SemanticScholarSource } from '../datasources/SemanticScholarSource';
+import { APIBasicAuthor } from '../models/API';
 import { BasicProfile } from '../models/profile/BasicProfile';
 import { ProfileService } from './ProfileService';
 
+
+
 export class SearchResultsService extends ProfileService {
-    build(query: string): BasicProfile[] {
-        query;
-        return [{} as BasicProfile];
+    private dataSource: DataSource = SemanticScholarSource.getInstance();
+    private apiBasicAuthors: APIBasicAuthor[];
+    private basicProfiles: BasicProfile[];
+    private pictureURL: string;
+    async build(query: string): BasicProfile[] {
+        this.apiBasicAuthors = await this.dataSource.fetchSearchResults(query);
+        
+        for(const basicAuthors of this.apiBasicAuthors){
+            this.pictureURL = await this.dataSource.fetchPictureURL(basicAuthors.authorId);
+            this.basicProfiles.push(new BasicProfile(basicAuthors.authorId, basicAuthors.name, basicAuthors.affiliations, +basicAuthors.citationCount, this.pictureURL));
+        }
+        return this.basicProfiles;
     }
 }
