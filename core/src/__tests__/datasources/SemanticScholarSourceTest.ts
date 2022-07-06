f2596e72263657bcb0fba8db8ec7c88531c40448
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-07-06 15:45:58 +0200

    Implement SemanticScholarSourceTests

--- core/src/__tests__/datasources/SemanticScholarSourceTest.ts
+++ core/src/__tests__/datasources/SemanticScholarSourceTest.ts
@@ -3,43 +3,57 @@ import { Article } from '../../models';
 
 describe('findOrCreate method', () => {
     // it('fetches author ids', async () => {
-    //     const authorsIds: string[] = await SemanticScholarSource.getInstance().fetchAuthorIds('walter tichy');
+    //     const basicAuthors: APIBasicAuthor[] = await SemanticScholarSource.getInstance().fetchSearchResults('walter tichy');
+    //     const authorIDs: string[] =[];
+    //     for(const basicAuthor of basicAuthors){
+    //          authorIds.push(basicAuthor.authorId)
+    //}
     //     expect(authorsIds).toStrictEqual(['143732150', '1679754', '1395637352', '89914629']);
     // }, 30000);
-    //it('fetches the correct first paper', async () => {
-    //const articles: Article[] = await SemanticScholarSource.getInstance().fetchArticles('1679754');
-    //expect(articles[0].title).toBe('Software Architectures (Dagstuhl Seminar 9508)');
+
+    //it('fetches an author', async () => {
+    //const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //expect(auhtors.length > 0).toBe(true);
     //}, 30000);
-    // TODO: Write a meaningful test case that doesn't break on api data changes
-    //it('fetches the correct citation count', async () => {
-    //const citation: number = await SemanticScholarSource.getInstance().fetchCitation('1679754');
-    //expect(citation).toBe(7903);
+
+    //it('fetches the correct first paper', async () => {
+    //const articles: APIPaper[] = await SemanticScholarSource.getInstance().fetcPapers('1679754');
+    //expect(articles.length > 0).toBe(true);
+    //}, 30000);
+
+    //it('fetchs the citation count of author', async () => {
+    //const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //expect(authors[0].citationCount != null).toBe(true);
     //});
-    // it('fetches the correct h-Index', async () => {
-    //     const hIndex: number = await SemanticScholarSource.getInstance().fetchHIndex('1679754');
-    //     expect(hIndex).toBe(39);
+
+    // it('fetches a h-Index', async () => {
+    //const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //     expect(authors[0].hIndex != null).toBe(true);
     // }, 30000);
-    // it('fetches author affiliation', async () => {
-    //     const affiliation: string[] = await SemanticScholarSource.getInstance().fetchAffiliations('1679754');
-    //     expect(affiliation.length).toBe(0);
+
+    // it('fetches an author affiliation', async () => {
+    //const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //     expect(authors[0].affiliation.length > 0).toBe(true);
     // }, 30000);
-    // TODO: Write a meaningful test case that doesn't break on api data changes
-    //it('fetches author citation', async () => {
-    //const citation: number = await SemanticScholarSource.getInstance().fetchCitation('1679754');
-    //expect(citation).toBe(7903);
+
+    //it('fetches paper reference count', async () => {
+    //const articles: APIPaper[] = await SemanticScholarSource.getInstance().fetcPapers('1679754');
+    //expect(articles[0].referenceCount > 0).toBe(true);
     //});
+
     // it('fetches author i10 index', async () => {
-    //     const i10Index: number = await SemanticScholarSource.getInstance().fetchI10Index('1679754');
-    //     expect(i10Index).toBe(null);
+    //  const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //     expect(authors[0].i10Index != null).toBe(true);
     // }, 30000);
+
     // it('fetches author articles', async () => {
-    //     const articles: Article[] = await SemanticScholarSource.getInstance().fetchArticles('1679754');
-    //     expect(articles.length).toBeGreaterThanOrEqual(200);
+    //     const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //     expect(authors[0].articles.length > 100).toBeGreaterThanOrEqual(true);
     // }, 30000);
-    // it('checks if author has self-citations', async () => {
-    //     const articles: Article[] = await SemanticScholarSource.getInstance().fetchArticles('1679754');
-    //     const hasSelfCitation: boolean = await SemanticScholarSource.getInstance().hasSelfCitation(articles[0], '1679754');
-    //     expect(hasSelfCitation).toBe(true);
+
+    // it('fetches website of author's page, async () => {
+    //     const authors: APIAuthor[] = await SemanticScholarSource.getInstance().fetchAuthor('1679754');
+    //     expect(authors[0].homepage != null).toBe(true);
     // });
     it('fetches author articles', async () => {
         expect(true).toBe(true);
