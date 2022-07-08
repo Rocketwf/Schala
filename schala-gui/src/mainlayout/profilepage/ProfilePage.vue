772c37fab221f063d3aa2f59e269aebf26040b71
Ayoub <unjde@student.kit.edu>
2022-07-08 02:38:45 +0200

    Fix profile summary reactivity

--- schala-gui/src/mainlayout/profilepage/ProfilePage.vue
+++ schala-gui/src/mainlayout/profilepage/ProfilePage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row justify-center bg-transparent">
         <div class="col-md-12 col-xs-12 self-center">
-            <profile-summary :profile="getFullProfile()" />
+            <profile-summary v-if="getFullProfile()" :profile="(getFullProfile() as FullProfile)" />
         </div>
     </div>
     <profile-content :profile-repr="getProfilePageStore().profileRepresentation as ProfileRepresentation" />
