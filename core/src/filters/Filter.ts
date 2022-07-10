714111f9221a0fa0eee79ee52d7852c69794c99f
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-07-10 22:32:02 +0200

    Comments Add Comments to Filter, Filterabl, ArticlesFilters, ChartOPtionsFilter

--- core/src/filters/Filter.ts
+++ core/src/filters/Filter.ts
@@ -1,8 +1,15 @@
 import { Filterable } from './Filterable';
 
 export abstract class Filter<S, T extends Filterable<T>> {
+    /**
+     * The value  of a filter
+     */
     protected _value: S;
 
+    /**
+     * Creates an instance of filter.
+     * @param value - value of the filter
+     */
     constructor(value: S) {
         this._value = value;
     }
@@ -10,13 +17,22 @@ export abstract class Filter<S, T extends Filterable<T>> {
     /**
      * Abstract method for applying a filter to the passed Model.
      */
-
     abstract apply(model: T): void;
+
+    /**
+     * Validates filter
+     * @param model - the given model
+     * @returns true if the given model is valid
+     */
     validate(model: T): boolean {
         model;
         return true;
     }
 
+    /**
+     * If the model is valid it calls apply method on the moodel
+     * @param model - the given model
+     */
     applyValidate(model: T): void {
         console.log(this.validate(model));
         if (this.validate(model)) this.apply(model);
--- core/src/filters/Filterable.ts
+++ core/src/filters/Filterable.ts
@@ -1,19 +1,51 @@
 import { Filter } from './Filter';
 
 export interface Filterable<T extends Filterable<T>> {
+    /**
+     * It creates a copy of this Filterable object
+     * @returns copy of Filterable object
+     */
     deepCopy(): T;
+    /**
+     * It applies all filters on Filterable object
+     */
     applyAllFilters(): void;
 
+    /**
+     * It represents Filter array of a Filterable object
+     */
     filters?: Filter<number | string | string[] | boolean, T>[];
+    /**
+     * It represents number of entries of a Filterable object
+     */
     entries: number;
 }
 
 export interface Paginable<T extends Filterable<T>> extends Filterable<T> {
+    /**
+     * It creates a copy of this Paginable object
+     * @returns copy of Paginable object
+     */
     deepCopy(): T;
+    /**s
+     * It applies all filters on Paginable object
+     */
     applyAllFilters(): void;
+    /**
+     * It applies all pagination filters on Paginable object
+     */
     applyPaginationFilter(): void;
 
+    /**
+     * It represents Filter array of a Paginable object
+     */
     filters?: Filter<number | string | string[] | boolean, T>[];
+    /**
+     * It represents pagination Filter array of a Paginable object
+     */
     paginationFilter?: Filter<number, T>;
+    /**
+     * It represents number of entries of a Paginable object
+     */
     entries: number;
 }
