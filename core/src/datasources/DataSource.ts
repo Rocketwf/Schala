ba219fa7f7d2491e1528b7299a36a441248e0f00
yagmur <yagmurdilaraakalin@gmail.com>
2022-07-11 02:59:19 +0200

    Add comments to DataSource

--- core/src/datasources/DataSource.ts
+++ core/src/datasources/DataSource.ts
@@ -1,6 +1,18 @@
 import { BasicProfile, FullProfile } from '../models';
 
+/**
+ * Interface defining the methods which will fetch search results and full profiles.
+ */
 export interface DataSource {
+    /**
+     * Method for fetching the search results according the given query.
+     * @param query - query string
+     */
     fetchSearchResults(query: string): Promise<BasicProfile[]>;
+
+    /**
+     * Method for fetching full profiles of scholars according the given profile id.
+     * @param profileId - profile id of the scholars
+     */
     fetchFullProfile(profileId: string): Promise<FullProfile>;
 }
