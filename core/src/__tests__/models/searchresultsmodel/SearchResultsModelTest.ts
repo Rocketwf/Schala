d5acf5924654a47b79fefc6c6ff68ef65f2a0e84
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-07-13 01:15:10 +0200

    Implement SearchResultsModelTest

--- core/src/__tests__/models/searchresultsmodel/SearchResultsModelTest.ts
+++ core/src/__tests__/models/searchresultsmodel/SearchResultsModelTest.ts
@@ -1,7 +1,21 @@
-describe('chart options filter', () => 
+import { SemanticScholarSource } from '../../../datasources';
+import { BasicProfile, SearchResultsModel } from '../../../models';
+
+describe('SearchResultsModel tests', () => 
 {
-    it('passes', () => 
+    let basicProfiles: BasicProfile[];
+    let searchResultsModel: SearchResultsModel;
+    beforeEach(async () => 
     {
-        expect(true).toBe(true);
+        basicProfiles = await SemanticScholarSource.getInstance().fetchSearchResults('walter+f+tichy');
+        searchResultsModel = new SearchResultsModel(basicProfiles);
+    }, 30000);
+    it('checks deepCopy works right', () => 
+    {
+        expect(searchResultsModel.deepCopy().basicProfiles).toEqual(basicProfiles);
+    });
+    it('checks amount of entry', () => 
+    {
+        expect(searchResultsModel.entries).toBe(3);
     });
 });
