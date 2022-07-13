a59d5a1e2c3e00cef685c71df200f9ce5a0b67b1
Baris Kizilcelik <bukizilcelik@gmail.com>
2022-07-13 02:11:32 +0200

    Implement FullProfileServiceTest

---
+++ server/src/__tests__/services/FullProfileServiceTest.ts
@@ -0,0 +1,24 @@
+import { FullProfile } from '../../models/profile/FullProfile';
+import { FullProfileService } from '../../services/FullProfileService';
+
+describe('findOrCreate method', () => 
+{
+    let service: FullProfileService;
+    let profiles: FullProfile[];
+    beforeEach(async () => 
+    {
+        service = new FullProfileService();
+        profiles = await service.build('1679754');
+    }, 30000);
+    
+    it('builds a fullprofile array', async () => 
+    {
+        expect(profiles.length > 0).toBe(true);
+    }, 30000);
+    it('checks if the name is correct', async () => 
+    {
+        expect(profiles[0].basicProfile.name).toBe('Walter F Tichy');
+    }, 30000);
+    
+
+});
