7a2c41a82d6381a2a72d6ad27f7d826d51542a54
yagmur <yagmurdilaraakalin@gmail.com>
2022-07-14 01:18:31 +0200

    Implement BasicBarsChartCardTest

---
+++ schala-gui/src/__tests__/sharedcomponents/cards/graphchartcards/BasicBarsChartCardTest.ts
@@ -0,0 +1,53 @@
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { beforeEach, describe, expect, it } from '@jest/globals';
+import { mount, VueWrapper } from '@vue/test-utils';
+import BasicBarsChartCard from '../../../../sharedcomponents/cards/graphchartcards/BasicBarsChartCard.vue';
+import { BasicBarsChartModel, Series, ViewName } from 'schala-core';
+import SimpleCard from '../../../../sharedcomponents/cards/SimpleCard.vue';
+
+installQuasarPlugin();
+
+const labels = ['label1', 'label2'];
+const series: Series[] = [
+    new Series('2020', [3, 5, 50]),
+    new Series('2021', [5, 70]),
+    new Series('2022', [20, 35]),
+    new Series('2019', [10, 3, 6, 22, 14]),
+];
+const mockCardModel = new BasicBarsChartModel('Title', '', ViewName.BasicBarsChartCard, 10, series, 'x-title', 'y-title', labels);
+
+describe('BasicBarsChartCard', () =>
+{
+    let wrapper: VueWrapper<any>;
+    beforeEach(() =>
+    {
+        wrapper = mount(BasicBarsChartCard, {
+            props: {
+                cardModel: mockCardModel,
+            }
+        });
+    });
+
+    it('renders', () =>
+    {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+
+    it('has correct attributes', () =>
+    {
+        const wrapper = mount(BasicBarsChartCard, {
+            props: {
+                cardModel: mockCardModel,
+            }
+        });
+        expect(wrapper.text()).toEqual('Title');
+
+    });
+
+    it('does simple card exist', () =>
+    {
+        const simpleCard = wrapper.findComponent(SimpleCard);
+        expect(simpleCard.exists()).toBe(true);
+    }) ;
+});
