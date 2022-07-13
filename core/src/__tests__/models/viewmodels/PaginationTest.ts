2e67a36c7e26b45d3cf85521be051072b6b43096
ulas uyanik <ulasuyanik.35@gmail.com>
2022-07-13 22:53:24 +0200

    Write tests for ProfileRepr. and Pagination

--- core/src/__tests__/models/viewmodels/PaginationTest.ts
+++ core/src/__tests__/models/viewmodels/PaginationTest.ts
@@ -1,34 +1,76 @@
-import { ArticlesPaginationFilter } from '../../../filters/articlesfilters/ArticlesFilter';
+import { Filter } from '../../../filters';
+import { ArticlesFilter, ArticlesPaginationFilter, SortByFilter } from '../../../filters/articlesfilters/ArticlesFilter';
 import { Article, ArticleCoAuthor, ArticlesModel, Pagination, ViewName } from '../../../models';
 
-describe('pagination filter', () => 
+let articlePagination: Pagination<ArticlesModel>;
+let artModel: ArticlesModel;
+beforeAll( () => 
 {
-    it('filter', () => 
+    const articlePaginationFilter: ArticlesPaginationFilter = new ArticlesPaginationFilter(15);
+    const coAuthors:ArticleCoAuthor = new ArticleCoAuthor('4242424','Georgios Zervakis');
+    const article:Article = new Article(
+        'NP=P ?',
+        'Karlsruhe Institute of Technology',
+        2022,
+        12,
+        '',
+        [coAuthors],
+        'My proof is perfect'
+    );
+    const articles:Article[] = new Array<Article>();
+    articles.push(article);
+    const articlesModel: ArticlesModel = new ArticlesModel(
+        articles,
+        'Articles',
+        '',
+        ViewName.ArticlesCard,
+        12,
+    );
+    articlesModel.paginationFilter = articlePaginationFilter;
+    const articlesFilter:SortByFilter = new SortByFilter('year');
+    articlesModel.filters = [articlesFilter];
+    articlesModel.pagination = new Pagination(articlePaginationFilter,articlesModel);
+    const ap: Pagination<ArticlesModel> = new Pagination(articlePaginationFilter, articlesModel);
+    ap.paginationFilter = articlePaginationFilter;
+    articlePagination=ap;
+    artModel=articlesModel;
+});
+
+describe('Set model', () =>
+{
+    it('Check if model of articlePagination is set correctly', () => 
     {
-        const articlePaginationFilter: ArticlesPaginationFilter = new ArticlesPaginationFilter(15);
-        const coAuthors:ArticleCoAuthor = new ArticleCoAuthor('','');
-        const article:Article = new Article(
-            '',
-            '',
-            2000,
-            12,
-            '',
-            [coAuthors],
-            ''
-        );
-        const articles:Article[] = new Array<Article>();
-        articles.push(article);
-        const articlesModel: ArticlesModel = new ArticlesModel(
-            articles,
-            'Articles',
-            '',
-            ViewName.ArticlesCard,
-            12,
-        );
-        const articlePagination: Pagination<ArticlesModel> = new Pagination(articlePaginationFilter, articlesModel);
-        articlePagination.maxPage;
-        articlePagination.currentPage;
-        articlePagination.paginationFilter;
-        articlePagination.model;
+        expect(articlePagination.model).toEqual(artModel);
+    });
+});
+
+describe('Hits per page', () =>
+{
+    it('Default hits per page', () => 
+    {
+        expect(artModel.paginationFilter.hitsPerPage).toBe(10);
+    });
+    
+    it('Change hits per page', () => 
+    {
+        artModel.updateHitsPerPage(20);
+        expect(artModel.paginationFilter.hitsPerPage).toBe(20);
+    });
+});
+
+describe('Handle switch', () =>
+{
+    it('Change max page', () => 
+    {
+        artModel.pagination.handleSwitch();
+        expect(artModel.pagination.maxPage).toBe(1);
+    });
+});
+
+describe('Current page', () =>
+{
+    it('Model current page', () =>
+    {
+        expect(artModel.pagination.currentPage).toBe(15);
     });
 });
--- core/src/__tests__/representations/ProfileRepresentationTest.ts
+++ core/src/__tests__/representations/ProfileRepresentationTest.ts
@@ -7,7 +7,7 @@ beforeAll(() =>
 {
     const bp: BasicProfile = new BasicProfile(
         '1679754',
-        'W. Fahrettin Tichy',
+        'Walter Tichy',
         ['Karlsruhe Institute of Technology'],
         7693,
         270,
@@ -15,7 +15,7 @@ beforeAll(() =>
     );
     const pbyArray:Array<PublicationByYear> = new Array<PublicationByYear>();
     const cbyArray:Array<CitationByYear> = new Array<CitationByYear>();
-    for(let i: number = 1975 ; i < 1985; i++)
+    for(let i: number = 1975 ; i <= 2022; i++)
     {
         const pby:PublicationByYear = new PublicationByYear(i,5);
         pbyArray.push(pby);
@@ -23,16 +23,16 @@ beforeAll(() =>
         cbyArray.push(cby);
     }
     const pbvArray:Array<PublicationByVenue> = new Array<PublicationByVenue>();
-    const pbv:PublicationByVenue = new PublicationByVenue('KIT',10000);
+    const pbv:PublicationByVenue = new PublicationByVenue('KIT',9400);
     pbvArray.push(pbv);
     const citedScholars:Array<CitedScholar> = new Array<CitedScholar>();
-    citedScholars.push(new CitedScholar('Yigit',31));
+    citedScholars.push(new CitedScholar('Yigit Oguz',42));
     const authors: Array<Author> = new Array<Author>();
     authors.push(new Author('Om Prakash',100,11));
     const articles:Array<Article> = new Array<Article>();
     const coAuthors: Array<ArticleCoAuthor> = new Array<ArticleCoAuthor>();
     coAuthors.push(new ArticleCoAuthor('4242422','Georgios Zervakis'));
-    articles.push(new Article('Turing machine simulator','KIT',1977,15,'',coAuthors,'Alan Turing'));
+    articles.push(new Article('Turing machine simulator','KIT',1977,15,'',coAuthors,'Alan Turing is genius'));
     const fp: FullProfile = new FullProfile(
         ['Computer Science','Mathematics','Engineering'],
         31,
@@ -41,7 +41,7 @@ beforeAll(() =>
         60,
         3031,
         1340,
-        10000,
+        9400,
         'https://ps.ipd.kit.edu/',
         bp,
         pbyArray,
@@ -54,17 +54,37 @@ beforeAll(() =>
     profileRepresentation = new ProfileRepresentation(fp);
 });
 
-describe('check card id validity', () => 
+describe('check cards for width validity', () => 
 {
     it('for loop for cards', () => 
     {
+        let isInvalid: boolean = false;
         profileRepresentation.renderProfile();
         profileRepresentation.rowModels.forEach((rowModel: RowModel) => 
         {
             rowModel.simpleCardModels.forEach((simpleCardModel: SimpleCardModel) => 
             {
-                expect(simpleCardModel.id).not.toBe(null);
+                if(simpleCardModel.colWidth <= 0)
+                {
+                    isInvalid = true;
+                }
             });
         });
+        expect(isInvalid).toBe(false);
+    });
+
+    it('check row id validity', () => 
+    {
+        let isInvalid: boolean = false;
+        profileRepresentation.renderProfile();
+        for(const rowModel of profileRepresentation.rowModels)
+        {
+            console.log(rowModel.id);
+            if(rowModel.id == null)
+            {
+                isInvalid=true;
+            }
+        }
+        expect(isInvalid).toBe(false);
     });
 });
