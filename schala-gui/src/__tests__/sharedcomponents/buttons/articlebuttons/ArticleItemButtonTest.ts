09bd5301ebc56e41be2f9abf2f171c2056a9acc6
yagmur <yagmurdilaraakalin@gmail.com>
2022-07-14 20:40:22 +0200

    Implement ArticleItemButtonTest

---
+++ schala-gui/src/__tests__/sharedcomponents/buttons/articlebuttons/ArticleItemButtonTest.ts
@@ -0,0 +1,26 @@
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { beforeEach, describe, expect, it } from '@jest/globals';
+import { mount, VueWrapper } from '@vue/test-utils';
+import ArticleItemButton from '../../../../sharedcomponents/buttons/articlebuttons/ArticleItemButton.vue';
+
+installQuasarPlugin();
+
+const string = 'article';
+
+describe('ArticleItemButton', () =>
+{
+    let wrapper: VueWrapper<any>;
+    beforeEach(() =>
+    {
+        wrapper = mount(ArticleItemButton, {
+            props: {
+                icon: string,
+            }
+        });
+    });
+
+    it('renders', () =>
+    {
+        expect(wrapper.exists()).toBe(true);
+    });
+});
