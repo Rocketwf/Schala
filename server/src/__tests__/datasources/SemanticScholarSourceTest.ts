3f3cc1c9996ac661cb12f9fabe0072e11bc5d0cc
Ayoub <unjde@student.kit.edu>
2022-07-14 21:36:07 +0200

    Make tests work with the caching feature

--- server/src/__tests__/datasources/SemanticScholarSourceTest.ts
+++ server/src/__tests__/datasources/SemanticScholarSourceTest.ts
@@ -3,19 +3,25 @@ import { APIBasicAuthor, APIAuthor, APIPaper } from '../../models/API';
 
 describe('findOrCreate method', () => 
 {
+    let dataSource: SemanticScholarSource;
+    beforeEach(() => 
+    {
+        dataSource = new SemanticScholarSource();
+        dataSource.destruct();
+    });
     it('fetches basic authors', async () => 
     {
-        const basicAuthors: APIBasicAuthor[] = await new SemanticScholarSource().fetchSearchResults('walter tichy');
+        const basicAuthors: APIBasicAuthor[] = await dataSource.fetchSearchResults('walter tichy');
         expect(basicAuthors.length > 0).toBe(true);
-    }, 30000); 
+    }, 30000);
     it('fetches author ids', async () => 
     {
-        const author: APIAuthor = await new SemanticScholarSource().fetchAuthor('1679754');
+        const author: APIAuthor = await dataSource.fetchAuthor('1679754');
         expect(author.aliases.length).toBe(5);
     }, 30000);
     it('fetches papers', async () => 
     {
-        const papers: APIPaper[] = await new SemanticScholarSource().fetchPapers([
+        const papers: APIPaper[] = await dataSource.fetchPapers([
             'e64bc2637e4aff5099506a6fb4725dc2543b3a1b',
             'e0a42ebc52f83d59da052a07db57448ff8045304',
         ]);
@@ -23,30 +29,26 @@ describe('findOrCreate method', () =>
     }, 30000);
     it('checks error message of fetchAuthor', async () => 
     {
-        const sem: SemanticScholarSource = new SemanticScholarSource();
-        try
+        try 
         {
-            await sem.fetchAuthor('0000000000000');
+            await dataSource.fetchAuthor('0000000000000');
             expect(true).toBe(false);
-        } 
-        catch (error)
+        }
+        catch (error) 
         {
             expect(error.message).toBe('Request failed with status code 404');
-        }  
-        
+        }
     }, 30000);
     it('checks error message of fetchSearchResults', async () => 
     {
-        const sem: SemanticScholarSource = new SemanticScholarSource();
-        try
+        try 
         {
-            await sem.fetchSearchResults('');
+            await dataSource.fetchSearchResults('');
             expect(true).toBe(false);
-        } 
-        catch (error)
+        }
+        catch (error) 
         {
             expect(error.message).toBe('Request failed with status code 400');
         }
     }, 30000);
-    
 });
