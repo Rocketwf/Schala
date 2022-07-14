9b1d8d303c990487a907e0d178f98730ca38c69b
ulas uyanik <ulasuyanik.35@gmail.com>
2022-07-14 17:19:46 +0200

    Write tests for PopupEditButton

--- core/src/__tests__/models/inputs/PopupEditButtonTest.ts
+++ core/src/__tests__/models/inputs/PopupEditButtonTest.ts
@@ -3,19 +3,21 @@ import { RangeButton, ShowingButton } from '../../../models/inputs/PopupEditButt
 import { FromFilter, ShowingFilter, ToFilter } from '../../../filters/objectserieschartfilters/ObjectSeriesFilter';
 import { Filter } from '../../../filters/Filter';
 
-let rangeButton : RangeButton;
-let showingButton:ShowingButton;
+let rangeButton: RangeButton;
+let showingButton: ShowingButton;
 
 describe('Range Button Filter Test', () => 
 {
     it('Filter Test', () => 
     {
         const series: Array<Series> = new Array<Series>();
-        series.push(new Series(2018 + '', [20, 30, 20]));
-        series.push(new Series(2019 + '', [30, 40, 30]));
-        series.push(new Series(2020 + '', [40, 50, 40]));
-        const fromFilter: Filter<number, StackedColumnsChartModel> = new FromFilter(3);
-        const toFilter: Filter<number, StackedColumnsChartModel> = new ToFilter(3);
+        series.push(new Series('2017', [10, 20, 10]));
+        series.push(new Series('2018', [20, 30, 20]));
+        series.push(new Series('2019', [30, 40, 30]));
+        series.push(new Series('2020', [40, 50, 40]));
+        series.push(new Series('2021', [50, 60, 50]));
+        const fromFilter: Filter<number, StackedColumnsChartModel> = new FromFilter(2018);
+        const toFilter: Filter<number, StackedColumnsChartModel> = new ToFilter(2020);
         const objectSeriesChartModel: StackedColumnsChartModel = new StackedColumnsChartModel(
             'Citations by Year',
             'Walter Tichy',
@@ -24,7 +26,7 @@ describe('Range Button Filter Test', () =>
             series,
             'Years',
             'Number of citations',
-            ['indirect self-citations', 'self-citations', 'cited by others']
+            ['indirect self-citations', 'self-citations', 'cited by others'],
         );
         objectSeriesChartModel.filters = [fromFilter,toFilter];
         const fromNumberField: Field<number, StackedColumnsChartModel> = new Field<number, StackedColumnsChartModel>(
@@ -40,16 +42,24 @@ describe('Range Button Filter Test', () =>
             [objectSeriesChartModel]
         );
         const rangePopupEdit: RangeButton = new RangeButton('range', [fromNumberField, toNumberField]);
+        console.log(rangePopupEdit.label);
+        rangePopupEdit.label='';
+        rangePopupEdit.id='123321';
         rangeButton=rangePopupEdit;
-
+        rangePopupEdit.inputs;
         rangeButton.handleAll();
-        rangeButton.icon;
-        rangeButton.label;
-        rangeButton.id;
-        rangeButton.inputs;
-        rangeButton.id='';
-        rangeButton.label='';
+        expect(objectSeriesChartModel.series.length).toBe(3);
     });
+    it('Check icon', () =>
+    {
+        expect(rangeButton.icon).toBe('event');
+    }
+    );
+    it('Check id', () =>
+    {
+        expect(rangeButton.id).toBe('123321');
+    }
+    );
 });
 
 describe('Article Filter Button Test', () =>
@@ -57,34 +67,52 @@ describe('Article Filter Button Test', () =>
     it('Filter Test', () =>
     {
         const series: Array<Series> = new Array<Series>();
-        series.push(new Series('Gregor Snelting', [2000]));
-        const toFilter: Filter<number, BasicBarsChartModel> = new ShowingFilter(3);
-        const mcs: BasicBarsChartModel = new BasicBarsChartModel(
-            'Most cited scholars',
-            '',
-            ViewName.BasicBarsChartCard,
+        series.push(new Series('Gregor Snelting', [100]));
+        series.push(new Series('Walter Tichy', [150]));
+        series.push(new Series('Klemens Böhm', [50]));
+        series.push(new Series('Dorothea Wagner', [200]));
+        series.push(new Series('Stefan Kühnlein', [300]));
+        series.push(new Series('Thomas Worsch', [120]));
+        const showingFilter: Filter<number, StackedColumnsChartModel> = new ShowingFilter(5);
+        const objectSeriesChartModel: StackedColumnsChartModel = new StackedColumnsChartModel(
+            'Citations by Year',
+            'Walter Tichy',
+            ViewName.StackedColumnsChartCard,
             6,
             series,
+            'Years',
             'Number of citations',
-            '',
-            [],
+            ['indirect self-citations', 'self-citations', 'cited by others'],
         );
-        mcs.filters=[toFilter];
+        objectSeriesChartModel.filters=[showingFilter];
         const showingNumberField: Field<number, BasicBarsChartModel> = new Field<number, BasicBarsChartModel>(
             'showing',
-            3,
-            toFilter,
-            [mcs],
+            5,
+            showingFilter,
+            [objectSeriesChartModel],
         );
         const showingPopupEdit: ShowingButton = new ShowingButton('range', [showingNumberField]);
+        console.log(showingPopupEdit.label);
+        showingPopupEdit.id='321123';
         showingButton = showingPopupEdit;
         showingButton.handleAll();
-        showingButton.icon;
-        showingButton.id;
         showingButton.inputs;
-        showingButton.label;
-        showingButton.id='';
-        showingButton.label='';
-        showingButton.icon='';
+        expect(objectSeriesChartModel.series.length).toBe(5);
     });
+    it('Check icon', () =>
+    {
+        expect(showingButton.icon).toBe('people');
+    }
+    );
+    it('Showing icon', () =>
+    {
+        showingButton.icon='event';
+        expect(showingButton.icon).toBe('event');
+    }
+    );
+    it('Check id', () =>
+    {
+        expect(showingButton.id).toBe('321123');
+    }
+    );
 });
