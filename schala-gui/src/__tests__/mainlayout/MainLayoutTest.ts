526affc54a88f966fcd2fa85b82d753d3982f70d
yagmur <yagmurdilaraakalin@gmail.com>
2022-07-14 20:35:24 +0200

    Implement MainLayoutTest and add pinia-testing

---
+++ schala-gui/src/__tests__/mainlayout/MainLayoutTest.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from '@jest/globals';
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { shallowMount } from '@vue/test-utils';
+import MainLayout from '../../mainlayout/MainLayout.vue';
+import { createTestingPinia } from '@pinia/testing';
+
+installQuasarPlugin();
+
+const wrapper = shallowMount(MainLayout, {
+    global: {
+        plugins: [createTestingPinia()],
+    },
+});
+
+describe('MainLayout Test', () =>
+{
+    it('mounts without errors', () =>
+    {
+        expect(wrapper).toBeTruthy();
+    });
+
+});
