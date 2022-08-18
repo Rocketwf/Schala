101755001bda13a73a27c15fffdea2062cd9ba8a
ulas uyanik <ulasuyanik.35@gmail.com>
2022-08-18 17:53:39 +0200

    Write test for profile and route classes of server

--- server/src/__tests__/routes/SearchResultsRoutesTest.ts
+++ server/src/__tests__/routes/SearchResultsRoutesTest.ts
@@ -4,10 +4,11 @@ import { SearchResultsRoutes } from '../../routes/SearchResultsRoutes';
 
 describe('SearchResultsRoutes test', () => 
 {
-    it('checks length of configureRoutes', () => 
+    it('checks length of configureRoutes and get/set name', async () => 
     {
         const app: Application = express();
         const profile: SearchResultsRoutes = new SearchResultsRoutes(app);
-        expect(profile.configureRoutes().length > 0).toBe(true);
+        profile.name = 'search';
+        expect(profile.configureRoutes().length > 0 && profile.name == 'search').toBe(true);
     }, 30000);
 });
